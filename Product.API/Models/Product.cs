e87de394c981780fddb36530444306f50f47ed5e
Ryan Nemeth <ryannemeth@live.com>
2022-02-25 21:03:25 -0500

    initial commit

---
+++ Product.API/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Merchandise.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+    }
+}
